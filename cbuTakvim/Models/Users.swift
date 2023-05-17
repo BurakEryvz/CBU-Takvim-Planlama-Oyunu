dbf5bbd598bcb105563e17965cabaf1c876b0099
BurakEryvz <burak.eryavuz5757@icloud.com>
2023-05-17 14:05:41 +0300

    Firebase Auth ve Firebase firestore eklendi ve islemleri tamamlandi.

---
+++ cbuTakvim/Models/Users.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  cbuTakvim
+//
+//  Created by Burak Eryavuz on 16.05.2023.
+//
+
+import Foundation
+
+
+struct User {
+    
+    var uID:String
+    var firstName:String
+    var lastName:String
+    
+    
+    
+    
+    
+}
