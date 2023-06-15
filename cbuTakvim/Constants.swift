ca02775c42bf22a2e2e40666e57ccdedcb51b608
BurakEryvz <burak.eryavuz5757@icloud.com>
2023-06-15 18:34:00 +0300

    Kod yazimi alarm haric tamamlandi.

---
+++ cbuTakvim/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  cbuTakvim
+//
+//  Created by Burak Eryavuz on 17.05.2023.
+//
+
+import Foundation
---
+++ cbuTakvim/Models/Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  cbuTakvim
+//
+//  Created by Burak Eryavuz on 20.05.2023.
+//
+
+import Foundation
